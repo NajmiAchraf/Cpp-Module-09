02c6a79e7c0b84ed888f7ad14f3d2109ce8b266c
NajmiAchraf <najmi.achraf@gmail.com>
2023-03-19 18:47:20 +0000

    On branch master

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,20 +3,18 @@
 bool run(int ac) {
 	if (ac != 2)
 		return false;
-
 	return true;
 }
 
 int main(int ac, char **av) {
 	if (run(ac)) {
-
 		try {
 			RPN btc(av[1]);
 		} catch (const std::exception &e) {
 			cerr << e.what() << endl;
 		}
 	} else {
-		cout << "Error: could not open file." << endl;
+		cout << "Error" << endl;
 	}
 
 	return 0;
